44dc4d3e1e0ee32472ec078817eba999711214f4
Eray Altun <eray_altun_25@hotmail.com>
2023-03-14 19:34:59 +0300

    Bezier eğrisi izdüşümü bulma
    Bezier eğrisi izdüşümü bulma

---
+++ Bilgisayar Grafikleri Ödev/Bilgisayar_Grafikleri_Odev/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Bilgisayar_Grafikleri_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(93, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(88, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kontrol Noktaları";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(93, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kontrol Noktaları";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(400, 52);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(260, 340);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(395, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kontrol Noktaları İzometrik";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(400, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Kontrol Noktaları İZOMETRİK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(740, 52);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(420, 788);
+            this.listBox3.TabIndex = 6;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(1202, 52);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(311, 788);
+            this.listBox4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(735, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Bezier Y. N. Bulutu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(1197, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(316, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Bezier Y. N. Bulutu İZOMETRİK";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(740, 867);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(773, 82);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Bezier Yüzey Nokta Bulutu // İZOMETRİK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1924, 1013);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "SpY";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Bilgisayar Grafikleri Ödev/Bilgisayar_Grafikleri_Odev/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bilgisayar_Grafikleri_Odev
+{
+    public partial class Form1 : Form
+    {
+      
+        double[,] XCisimNoktaBezierYuzeyKontrolN = new double[20, 4] {  { 0.3, 0.1, 1.5, 1 },
+                                                                        { 0.2, 1.0, 1.0, 1 },
+                                                                        { 0.2, 0.7, 0.8, 1 },
+                                                                        { 0.3, 1.0, 0.2, 1 },
+
+                                                                        { 0.5, 0.8, 1.4, 1 },
+                                                                        { 0.6, 0.6, 1.1, 1 },
+                                                                        { 0.5, 1.2, 0.8, 1 },
+                                                                        { 0.5, 1.0, 0.1, 1 },
+
+                                                                        { 1.2, 1.8, 1.6, 1 },
+                                                                        { 1.1, 0.6, 1.0, 1 },
+                                                                        { 1.2, 1.2, 0.7, 1 },
+                                                                        { 1.3, 1.0, 0.3, 1 },
+
+                                                                        { 1.5, 1.8, 1.5, 1 },
+                                                                        { 1.4, 0.6, 1.0, 1 },
+                                                                        { 1.1, 1.2, 0.7, 1 },
+                                                                        { 0.3, 1.0, 0.1, 1 },
+
+                                                                        { 1.8, 0.4, 1.5, 1 },
+                                                                        { 1.8, 1.6, 1.1, 1 },
+                                                                        { 1.8, 0.7, 0.8, 1 },
+                                                                        { 0.3, 1.0, 0.2, 1 }};
+
+        double[,] XCisimNoktaBezierYuzeyKontrolNizometrik = new double[20, 4];
+
+        double[,] Tizometrik = new double[4, 4] { {0.707,  -0.408, 0, 0 },
+                                                  {0,       0.816, 0, 0 },
+                                                  {-0.707, -0.408, 0, 0 },
+                                                  {0,       0,     0, 0 } };
+
+        double[,] BezierNoktaBulutuizometrik = new double[64, 4];
+
+        double[,] BezierYuzeyNoktaBulutu = new double[64, 4];
+
+
+        public Form1()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+
+            listBox1.Items.Add("#\t" + "x\t" + "y\t" + "z\t");
+
+            for (int i = 0; i < 20; i++)
+
+            {
+
+                if (i % 3 == 0) listBox1.Items.Add("");
+
+                listBox1.Items.Add(i + "\t" + XCisimNoktaBezierYuzeyKontrolN[i, 0] + "\t" + XCisimNoktaBezierYuzeyKontrolN[i, 1] + "\t" + XCisimNoktaBezierYuzeyKontrolN[i, 2]);
+
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+
+            for (int i = 0; i < 20; i++)
+
+            {
+
+                for (int j = 0; j < 4; j++)
+
+                {
+
+                    for (int k = 0; k < 4; k++)
+
+                    {
+
+                        XCisimNoktaBezierYuzeyKontrolNizometrik[i, j] += XCisimNoktaBezierYuzeyKontrolN[i, k] * Tizometrik[k, j];
+
+                    }
+
+                    XCisimNoktaBezierYuzeyKontrolNizometrik[i, j] = Math.Round(XCisimNoktaBezierYuzeyKontrolNizometrik[i, j], 4);
+
+                }
+
+            }
+
+            listBox2.Items.Add("#\t" + "x\t" + "y\t" + "z\t");
+
+            for (int i = 0; i < 20; i++)
+
+            {
+
+                if (i % 3 == 0) listBox2.Items.Add("");
+
+                listBox2.Items.Add(i + "\t" + XCisimNoktaBezierYuzeyKontrolNizometrik[i, 0] + "\t" + XCisimNoktaBezierYuzeyKontrolNizometrik[i, 1] + "\t" + XCisimNoktaBezierYuzeyKontrolNizometrik[i, 2] + "\t" + XCisimNoktaBezierYuzeyKontrolNizometrik[i, 2]);
+
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+
+        {
+
+            double hassasiyet = 0.25, u = 0, w;
+
+            listBox3.Items.Add("#\t" + "u\t" + "w\t" + "x\t" + "y\t" + "z\t");
+
+            for (int i = 0; i < 8; i++)
+
+            {
+
+                w = 0;
+
+                for (int j = 0; j < 8; j++)
+
+                {
+
+                    BezierYuzeyNoktaBulutu[i * 8 + j, 0] = Math.Round(BezierHesapla(u, w, 0), 4);
+
+                    BezierYuzeyNoktaBulutu[i * 8 + j, 1] = Math.Round(BezierHesapla(u, w, 1), 4);
+
+                    BezierYuzeyNoktaBulutu[i * 8 + j, 2] = Math.Round(BezierHesapla(u, w, 2), 4);
+
+                    if ((i * 8 + j) % 8 == 0) listBox3.Items.Add("");
+
+                    listBox3.Items.Add((i * 8 + j) + "\t" + u + "\t" + w + "\t" + BezierYuzeyNoktaBulutu[i * 8 + j, 0] + "\t" + BezierYuzeyNoktaBulutu[i * 8 + j, 1] + "\t" + BezierYuzeyNoktaBulutu[i * 8 + j, 2]);
+
+                    w += hassasiyet;
+
+                }
+
+                u += hassasiyet;
+
+            }
+    
+            for (int i = 0; i < 64; i++)
+
+            {
+
+                for (int j = 0; j < 4; j++)
+
+                {
+
+                    for (int k = 0; k < 4; k++)
+
+                    {
+
+                        BezierNoktaBulutuizometrik[i, j] += BezierYuzeyNoktaBulutu[i, k] * Tizometrik[k, j];
+
+                    }
+
+                    BezierNoktaBulutuizometrik[i, j] = Math.Round(BezierNoktaBulutuizometrik[i, j], 4);
+
+                }
+
+            }
+
+            listBox4.Items.Add("#\t" + "x\t" + "y\t" + "z\t");
+
+            for (int i = 0; i < 64; i++)
+
+            {
+
+                if (i % 8 == 0) listBox4.Items.Add("");
+
+                listBox4.Items.Add(i + "\t" + BezierNoktaBulutuizometrik[i, 0] + "\t" + BezierNoktaBulutuizometrik[i, 1] + "\t" + BezierNoktaBulutuizometrik[i, 2]);
+
+            }
+
+        }
+        private double BezierHesapla(Double uu, Double ww, int xyz)
+
+        {
+
+            double sonuc = 0, T = 0, K = 0;
+
+            int n = 4, m = 3;
+
+            for (int i = 0; i < 5; i++)
+
+            {
+
+                for (int j = 0; j < 4; j++)
+
+                {
+
+                    T = (Faktor(n) / (Faktor(i) * Faktor((n - i)))) * Math.Pow(uu, i) * Math.Pow((1 - uu), (n - i));
+
+                    K = (Faktor(m) / (Faktor(j) * Faktor((m - j)))) * Math.Pow(ww, j) * Math.Pow((1 - ww), (m - j));
+
+                    sonuc += XCisimNoktaBezierYuzeyKontrolN[i * 4 + j, xyz] * T * K;
+
+                }
+
+            }
+
+            return sonuc;
+
+        }
+        private double Faktor(double sayi)
+
+        {
+
+            double sonuc = 1;
+
+            for (int i = 1; i <= sayi; i++) sonuc *= (double)i;
+
+            return sonuc;
+
+        }
+
+    }
+
+}
